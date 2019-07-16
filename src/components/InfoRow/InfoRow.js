3e6f021559b3d8f06d98bef7c506a6bceea9132f
Joe Thomas <Emerick23@yahoo.com>
2019-07-16 16:58:41 -0500

    deploy check

--- src/components/InfoRow/InfoRow.js
+++ src/components/InfoRow/InfoRow.js
@@ -4,13 +4,7 @@ import './InfoRow.css'
 const InfoRow = () => {
     return (
         <div>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-3'>
-                        <h3 className='fluid'>Meet</h3>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     )
 }
