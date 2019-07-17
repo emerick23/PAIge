30709c0321073594bc7a03efe383384db6d93501
Joe Thomas <Emerick23@yahoo.com>
2019-07-17 11:11:31 -0500

    auth restriction

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
 import userService from '../../utils/userService'
 import './App.css';
 import LoginPage from '../LoginPage/LoginPage'
@@ -28,33 +28,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Router>
-            <Switch>
-              <Route exact path='/' render={() =>
-                <HomePage
-                  user={this.state.user}
-                  handleLogout={this.handleLogout}
-                />
-              } />
-              <Route exact path='/signup' render={({ history }) =>
-                <SignupPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              } />
-              <Route exact path='/login' render={({ history }) =>
-                <LoginPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              } />
-              <Route exact path='/content-library' render={() =>
-                <ContentPage
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() =>
+              <HomePage
                 user={this.state.user}
                 handleLogout={this.handleLogout}
-                />
-              } />
-              <Route exact path='/contact' render={({ history }) =>
+              />
+            } />
+            <Route exact path='/signup' render={({ history }) =>
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            } />
+            <Route exact path='/login' render={({ history }) =>
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            } />
+            <Route exact path='/content-library' render={() =>
+              <ContentPage
+                user={this.state.user}
+                handleLogout={this.handleLogout}
+              />
+            } />
+            <Route exact path='/contact' render={({ history }) =>
+              userService.getUser() ?
                 <ContactPage
                   history={history}
                   user={this.state.user}
@@ -62,9 +63,11 @@ class App extends Component {
                   contacts={this.state.contacts}
                   handleUpdateContacts={this.handleUpdateContacts}
                 />
+                :
+                <Redirect to='/login' />
               } />
-            </Switch>
-          </Router>
+          </Switch>
+        </Router>
       </div>
     );
   }
